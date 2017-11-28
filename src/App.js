430c74cec73a62b71dfe2dd855e5ca6a85ce8d2f
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-28 15:37:15 -0700

    Built constructor and set state to have data as empty object

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: {}
+    }
+  }
   render() {
     return (
       <div>Welcome To Headcount 2.0</div>
