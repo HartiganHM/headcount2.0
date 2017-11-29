3680d9bdc3aaf627fab50ab2a1b0a975d9dfef50
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-29 09:36:58 -0700

    Confirmed base level tests working for CardContainer

--- src/CardContainer/CardContainer.test.js
+++ src/CardContainer/CardContainer.test.js
@@ -3,3 +3,14 @@ import ReactDOM from 'react-dom';
 import CardContainer from './CardContainer';
 import { shallow, mount, render } from 'enzyme';
 
+describe('Card Container Tests', () => {
+    let renderedCardContainer;
+
+    beforeEach( () => {
+        renderedCardContainer = shallow(<CardContainer />)
+    });
+
+    it('CardContainer should exist', () => {
+        expect(renderedCardContainer).toBeDefined();
+    });
+})
