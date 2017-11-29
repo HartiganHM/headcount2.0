b35ead6987ad868501c792954bfeb5c4acbbde20
Matthew Renn <rennmatthewp@gmail.com>
2017-11-29 13:29:07 -0700

    adds key in iterator, adds PropTypes check, cleans formatting

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Card.css';
 
 const Card = ({ district, data }) => {
-    const districtData = Object.keys(data).map( year => {
-        return <li>{year}: {data[year]}</li>
-    })
-
+  const districtData = Object.keys(data).map(year => {
     return (
-        <div>
-            <span>{district}</span>
-            <ul>
-                {districtData}
-            </ul>
-        </div>
-    )
-}
+      <li key={year}>
+        {year}: {data[year]}
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <span>{district}</span>
+      <ul>{districtData}</ul>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  district: PropTypes.string,
+  data: PropTypes.object
+};
 
 export default Card;
