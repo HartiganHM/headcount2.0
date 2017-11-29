ea4b3510942575709022b68d833f0cf685c09924
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-29 12:44:23 -0700

    Pulled data into test environment and checked for rendered cards

--- src/CardContainer/CardContainer.test.js
+++ src/CardContainer/CardContainer.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CardContainer from './CardContainer';
 import { shallow, mount, render } from 'enzyme';
+import DistrictRepository from '../helper';
+import importedData from '../../data/kindergartners_in_full_day_program.js';
+
 
 describe('Card Container Tests', () => {
     let renderedCardContainer;
@@ -19,4 +22,13 @@ describe('Card Container Tests', () => {
     it('CardContainer should match the snapshot', () => {
         expect(renderedCardContainer).toMatchSnapshot();
     });
+
+    it('CardContainer should render Cards', () => {
+        data = new DistrictRepository(importedData);
+        renderedCardContainer = shallow(<CardContainer data={data}/>)
+
+        const expectedLength = 181;
+
+        expect(renderedCardContainer.find('Card').length).toEqual(expectedLength);
+    })
 })
