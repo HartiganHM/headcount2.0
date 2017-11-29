b26ff4fa5d2c905cc54854f7be1ff4a55b58ac7c
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-28 20:52:00 -0700

    Built out base level Search component

---
+++ src/Search/Search.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Search.css';
+
+class Search extends Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>Get at me, search!</div>
+        )
+    }
+}
+
+export default Search;
