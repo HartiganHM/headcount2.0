87a27eb3ba6fc0fd7cc5a1b9197760b05aa5c971
Matthew Renn <rennmatthewp@gmail.com>
2017-11-29 12:31:54 -0700

    cleans up formatting

--- src/CardContainer/CardContainer.js
+++ src/CardContainer/CardContainer.js
@@ -3,28 +3,22 @@ import PropTypes from 'prop-types';
 import Card from '../Card/Card';
 import './CardContainer.css';
 
-const CardContainer = ({data}) => {
-    if (data.data) {
+const CardContainer = ({ data }) => {
+  if (data.data) {
+    const allCards = Object.keys(data.data).map(district => {
+      const dist = data.data[district];
+      return (
+        <Card district={dist.location} data={dist.data} key={dist.location} />
+      );
+    });
 
-        const allCards = Object.keys(data.data).map( district => {
-            return (
-                <Card 
-                    district={data.data[district].location} data={data.data[district].data}
-                    key={data.data[district].location}
-                 />
-            )
-        })
-        
-        return (
-            <div>
-                {allCards}
-            </div>
-    )}
-    return null
-}
+    return <div>{allCards}</div>;
+  }
+  return null;
+};
 
 CardContainer.propTypes = {
-    data: PropTypes.object
-}
+  data: PropTypes.object
+};
 
-export default CardContainer
+export default CardContainer;
