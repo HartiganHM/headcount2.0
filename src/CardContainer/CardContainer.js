3ba2e5a8d6141c984599f959affa6e82a40e3a3b
Matthew Renn <rennmatthewp@gmail.com>
2017-11-29 11:58:34 -0700

    passes data into CardContainer, CardContainer maps the data creating a Card for each district

--- src/CardContainer/CardContainer.js
+++ src/CardContainer/CardContainer.js
@@ -2,13 +2,24 @@ import React from 'react';
 import Card from '../Card/Card'
 import './CardContainer.css';
 
-const CardContainer = () => {
-    return (
-        <div>
-            <span>I'm the card container, bruh</span>
-            <Card />
-        </div>
-    )
+const CardContainer = ({data}) => {
+    if (data.data) {
+
+        const allCards = Object.keys(data.data).map( district => {
+            return (
+                <Card 
+                    district={data.data[district].location} data={data.data[district].data}
+                    key={data.data[district].location}
+                 />
+            )
+        })
+        
+        return (
+            <div>
+                {allCards}
+            </div>
+    )}
+    return null
 }
 
 export default CardContainer
