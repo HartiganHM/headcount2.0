9df799b1414a2528088d9b8fb995779894749454
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-02 15:00:36 -0700

    Created all Comparison component files

---
+++ src/Comparison/Comparison.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import './CardContainer.css';
+
+const Comparison = () => {
+    return (
+        <div>
+            I'm a card comparererereer
+        </div>
+    )
+}
+
+export default Comparison;
