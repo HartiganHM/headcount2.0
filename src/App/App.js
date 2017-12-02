018418eab036c028e9de08f305ebea407aa3502c
Matthew Renn <rennmatthewp@gmail.com>
2017-12-02 13:47:01 -0700

    adds onClick to card, adds clicked card object to selectedArray

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import DistrictRepository from '../helper'
-import schoolData from '../../data/kindergartners_in_full_day_program.js'
+import DistrictRepository from '../helper';
+import schoolData from '../../data/kindergartners_in_full_day_program.js';
 import './App.css';
 
 import Search from '../Search/Search';
@@ -10,30 +10,49 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: {}
-    }
+      data: {},
+      selected: []
+    };
 
     this.helper = {};
     this.filterData = this.filterData.bind(this);
+    this.selectCard = this.selectCard.bind(this);
   }
 
   filterData(input) {
     let data = this.helper.findAllMatches(input);
-    this.setState({data})
+    this.setState({ data });
   }
 
   componentDidMount() {
     this.helper = new DistrictRepository(schoolData);
-    this.setState({ data: this.helper.data })
+    this.setState({ data: this.helper.data });
+  }
+
+  selectCard(card) {
+    console.log(this.helper.findByName(card.id))
+    let selectedCard = this.state.data[card.id];
+    let selected = [...this.state.selected, selectedCard];
+    if (this.state.selected.length < 2) {
+      this.setState({ selected });
+    }
+    console.log(this.state.selected)
   }
 
   render() {
     return (
-      <div className='app'>
-        <span className='header'>Headcount 2.0</span>
-        <Search filterCards={this.filterData}/>
-        <CardContainer data={this.state.data} />
-        <footer className='footer'>Developed by Matt Renn and Hugh Hartigan</footer>
+      <div className="app">
+        <span className="header">Headcount 2.0</span>
+        <Search filterCards={this.filterData} />
+        <CardContainer 
+          data={this.state.data}
+          selectCard={this.selectCard} 
+          selectedArray={this.state.selected}
+          />
+
+        <footer className="footer">
+          Developed by Matt Renn and Hugh Hartigan
+        </footer>
       </div>
     );
   }
--- src/CardContainer/CardContainer.js
+++ src/CardContainer/CardContainer.js
@@ -3,12 +3,20 @@ import PropTypes from 'prop-types';
 import Card from '../Card/Card';
 import './CardContainer.css';
 
-const CardContainer = ({ data }) => {
+const CardContainer = ({ data, selectCard, selectedArray }) => {
   if (data) {
+    let type = 'card';
+
     const allCards = Object.keys(data).map(district => {
       const dist = data[district];
+      selectedArray.forEach( ( obj ) => {
+        if ( obj.location === dist ) {
+          type = 'card selected'
+        }
+      })
+      
       return (
-        <Card district={dist.location} data={dist.data} key={dist.location} />
+        <Card district={dist.location} data={dist.data} key={dist.location} selectCard={selectCard} className={type}/>
       );
     });
 
