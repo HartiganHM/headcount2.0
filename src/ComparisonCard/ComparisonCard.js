d1947fb8031a6b8f6a264263c7974728612da91d
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-02 17:28:36 -0700

    Build ComparisonCard component for comparedData

---
+++ src/ComparisonCard/ComparisonCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import './ComparisonCard.css';
+
+const ComparisonCard = ({ comparedData }) => {
+    const keys = Object.keys(comparedData);
+    const data = keys.map( (district) => {
+        return comparedData[district]
+    })
+
+    const district1 = keys.length ? `${keys[0]}:${data[0]}` : 'District 1';
+    const district2 = keys.length ? `${keys[1]}:${data[1]}` : 'District 2';
+    const comparison = keys.length ? `${keys[2]}:${data[2]}` : 'District 2';
+
+    return(
+        <div>
+            <span>{district1}</span>
+            <span>{comparison}</span>
+            <span>{district2}</span>
+        </div>
+    )
+}
+
+export default ComparisonCard
