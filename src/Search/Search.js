443c75ef28e465bc5b093a8f992f549b9c699754
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-03 15:29:05 -0700

    Change to Icons directory and fix image paths

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import icon from '../Images/search.svg';
+import icon from '../Icons/search.svg';
 import './Search.css';
 
 class Search extends Component {
